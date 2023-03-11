f27c59a635a9ccee376f7d77e44d613b17947a1b
kiryl-f <filippovickirill81@gmail.com>
2023-03-11 18:07:45 +0300

    starting to implement necessary functionality

---
+++ resources/views/main_page.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>{{$title}}</title>
+</head>
+
+<body>
+    <div id="segment_1">
+        {{$segment_1['title']}}
+    </div>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/main', function () {
+   return view('main_page', ['title' => 'Main page', 'segment_1' => ['title' => 'Featured books']]);
+});
