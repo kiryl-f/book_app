3cb1e863a5c016a8b0b478ed1a10a7373ec9e0a5
kiryl-f <filippovickirill81@gmail.com>
2023-03-12 21:07:20 +0300

    created Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -1,7 +1,12 @@
 <?php
 
 namespace App\Models;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 
-class Book {
+class Book extends Model{
 
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
