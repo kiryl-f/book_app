caf535f3b609719793ebf27b95ed78e3238a4b2c
kiryl-f <filippovickirill81@gmail.com>
2023-04-07 17:36:00 +0300

    added some bootstrap

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -12,6 +12,7 @@ $name = User::find($id)->name;
 
 <head>
     <title>My profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('resources/css/nav_bar_style.css') }}" />
 </head>
 
