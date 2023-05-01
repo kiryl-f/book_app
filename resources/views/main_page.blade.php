bb66689b8dc8e3bd4fc71f2544193d0bedc03d9f
kiryl-f <filippovickirill81@gmail.com>
2023-05-01 23:43:16 +0300

    added some css

--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -9,11 +9,12 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="../css/general_padding.css" rel="stylesheet">
+    <link href="../css/login_panel_padding.css" rel="stylesheet">
 </head>
 
 <body>
 
-<div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 selection:bg-red-500 selection:text-white">
+<div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 selection:bg-red-500 selection:text-white" id="login-panel">
     @if (Route::has('login'))
         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
             @auth
